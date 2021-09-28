53f779459fb4ccf42663604f4566dadd3baee179
Sebastian Twarog <contact@stwarog.com>
2021-09-28 18:21:25 +0200

    Refactored training, added activity entity

--- src/Training.php
+++ src/Training.php
@@ -4,22 +4,13 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\Catalog\ExerciseId;
-use App\Exceptions\NotFoundException;
-use App\Repository\ExerciseById;
-
 final class Training
 {
     public function __construct(private TrainingId $id, Date $date, private PlanId $plan)
     {
     }
 
-    public function add(Weight $weight, Repeats $repeats, ExerciseId $id, ExerciseById $repo): void
+    public function add(Activity $exercise): void
     {
-        $exercise = $repo->findOne($id);
-
-        if (empty($exercise)) {
-            throw new NotFoundException('Exercise not found');
-        }
     }
 }
--- tests/Unit/TrainingTest.php
+++ tests/Unit/TrainingTest.php
@@ -4,15 +4,10 @@ declare(strict_types=1);
 
 namespace Unit;
 
-use App\Catalog\ExerciseId;
 use App\Date;
-use App\Exceptions\NotFoundException;
 use App\PlanId;
-use App\Repeats;
-use App\Repository\ExerciseById;
 use App\Training;
 use App\TrainingId;
-use App\Weight;
 use PHPUnit\Framework\TestCase;
 
 /** @covers \App\Training */
@@ -26,25 +21,5 @@ final class TrainingTest extends TestCase
             PlanId::random()
         );
         $this->assertInstanceOf(Training::class, $sut);
-    }
-
-    public function testAddNotExerciseFoundThrowsException(): void
-    {
-        // Except
-        $this->expectException(NotFoundException::class);
-        $this->expectExceptionMessage('Exercise not found');
-
-        // Given Training
-        $sut = new Training(TrainingId::random(), Date::now(), PlanId::random());
-
-        // And an excercise
-        $exerciseId = ExerciseId::random();
-
-        // And repo that returns null
-        $repo = $this->createMock(ExerciseById::class);
-        $repo->method('findOne')->with($exerciseId)->willReturn(null);
-
-        // When attempting to add new activity
-        $sut->add(new Weight(100), new Repeats(5), $exerciseId, $repo);
     }
 }
