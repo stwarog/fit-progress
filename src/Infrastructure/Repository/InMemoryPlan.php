a1a64638fe46cd63363fc92945d82f188fc24754
Sebastian Twarog <contact@stwarog.com>
2021-10-01 22:41:37 +0200

    Installed doctrine

--- src/Infrastructure/Repository/InMemoryPlan.php
+++ src/Infrastructure/Repository/InMemoryPlan.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Repository;
 
-use App\Domain\Date;
+use App\Domain\Catalog\Exercise;
+use App\Domain\Catalog\ExerciseId;
 use App\Domain\Name;
 use App\Domain\Plan;
 use App\Domain\PlanId;
 use App\Domain\Repository\PlanById;
-use App\Domain\TrainingId;
 
 final class InMemoryPlan implements PlanById
 {
     private array $catalog = [];
 
-    public function __construct(private PlanById $exists)
+    public function __construct()
     {
         $names = [
             ['4ef022ee-bd51-405e-b1a6-1234123', 'FBW'],
@@ -24,11 +24,9 @@ final class InMemoryPlan implements PlanById
         foreach ($names as $dataSet) {
             [$id, $name] = $dataSet;
             $this->catalog[$id] = new Plan(
-                new TrainingId($id),
+                new PlanId($id),
                 new Name($name),
-                $this->exists,
-                Date::now(),
-                PlanId::random()
+                [new Exercise(ExerciseId::random(), new Name('Pushes'))]
             );
         }
     }
