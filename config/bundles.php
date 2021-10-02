8ee558e49ac1c944fe74e78203c2c6703e3cda02
Sebastian Twarog <contact@stwarog.com>
2021-10-02 11:28:11 +0200

    Behat works

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,10 @@
 <?php
 
+use FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle;
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true]
 ];
