74025935ed53faffe59b3b40648a1185d9be22a4
Sebastian Twarog <contact@stwarog.com>
2021-10-11 22:15:09 +0200

    Query bus

---
+++ src/Infrastructure/Bus/MessengerQueryBus.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Bus;
+
+use App\Application\Query;
+use App\Application\QueryBus;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+use Throwable;
+
+final class MessengerQueryBus implements QueryBus
+{
+    private MessageBusInterface $messageBus;
+
+    public function __construct(MessageBusInterface $messageBus)
+    {
+        $this->messageBus = $messageBus;
+    }
+
+    /** @throws Throwable */
+    public function ask(Query $query): mixed
+    {
+        $envelope = $this->messageBus->dispatch($query);
+
+        /** @var HandledStamp $stamp */
+        $stamp = $envelope->last(HandledStamp::class);
+
+        return $stamp->getResult();
+    }
+}
